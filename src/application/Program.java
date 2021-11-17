2cb43a984056cd39f0e2f1b429a2df5120a68adf
unknown <marcio.jordao@hotmail.com>
2021-11-16 22:06:15 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
+
+
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Departament marcio = new Departament (1, "Books");
-		System.out.println(marcio);
+		Departament obj = new Departament (1, "Books");
+		
+		Seller seller = new Seller(33, "Marcio","marcio.jordao@hotmail.com", new Date(), 3700.0, obj);
+		
+		System.out.println(seller);
 		
 	}
 
